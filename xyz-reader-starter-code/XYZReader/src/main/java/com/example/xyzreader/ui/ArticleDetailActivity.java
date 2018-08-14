48fc8aab62277a66c5ab40f9677687d7fedccf76
Talles Oliveira <talles.oliveira2@gmail.com>
2018-08-13 22:25:48 -0300

    Small fixes in layout

--- xyz-reader-starter-code/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
+++ xyz-reader-starter-code/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
@@ -5,12 +5,10 @@ import android.app.FragmentManager;
 import android.app.LoaderManager;
 import android.content.Loader;
 import android.database.Cursor;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 
 import com.example.xyzreader.R;
@@ -20,7 +18,7 @@ import com.example.xyzreader.data.ItemsContract;
 /**
  * An activity representing a single Article detail screen, letting you swipe between articles.
  */
-public class ArticleDetailActivity extends ActionBarActivity
+public class ArticleDetailActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private Cursor mCursor;
